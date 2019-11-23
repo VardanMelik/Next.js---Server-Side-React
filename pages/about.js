55011ff303e1b44c674587fa279c2ccc92e3a4ce
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-11-23 12:14:33 +0400

    upload

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar'
+
+const About = () => (
+    <div>
+        <Navbar />
+        <h1>About Bitcoin Prices</h1>
+        <p>Application to view Bitcoin</p>
+    </div>
+)
+export default About
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar'
+
+
+const Index = () => (
+    <div>
+        <Navbar />
+        <h1>Welcome to Bitcoin Prices</h1>
+    </div>
+)
+
+export default Index
